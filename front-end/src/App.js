b2ff5d294fd2be5d55204b81debbbab71b487d33
mammothgazillionaire <mammothgazillionaire@gmail.com>
2018-12-04 18:49:43 +0530

    cipp component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CLIP from './components/CLIP';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+      <CLIP/>
       </div>
     );
   }
