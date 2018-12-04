89eab064064077090b4fa2deb3636dcbb20be335
mammothgazillionaire <mammothgazillionaire@gmail.com>
2018-12-05 03:20:19 +0530

    login signup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import DailyUpdates from './components/DailyUpdates';
 import CLIP from './components/CLIP';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 class App extends Component {
   render() {
@@ -9,6 +11,8 @@ class App extends Component {
       <div className="App">
         <Header />
         <CLIP/>
+        <Signup/>
+        {/* <Login/> */}
         <DailyUpdates />
       </div>
     );
