e930cd74b33107678f9d3c57a39f72bc802cd630
mammothgazillionaire <mammothgazillionaire@gmail.com>
2018-12-05 11:03:18 +0530

    user authentication

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CLIP from './CLIP';
+import * as data from './../data.json';
 
-const Login = () => {
-  return (
-   <React.Fragment>
-     <CLIP/>
-     <div className='login'>
-      <form className="login__form">
-        <input type="text" className="login__email" placeholder="enter your email"/><br/>
-        <input type="text" className="login__password" placeholder="enter your password"/><br/>
-        <button className="login__btn">Login</button>
-      </form>
-    </div>
-   </React.Fragment>
+
+class Login extends Component {
+  
+  state = {
+    username : '',
+    password : ''
+  }
+
+handleSubmit = (e) => {
+  e.preventDefault();
+  const {username,password} = this.state;
+  const currentUser = data.default.filter(user => username === user.first_name);
+  console.log(currentUser)
+}
+
+  hanldeChange = (e) => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
+  render() {
+    console.log(data);
+    return (
+    <React.Fragment>
+      <CLIP/>
+      <div className='login'>
+        <form className="login__form" onSubmit={this.handleSubmit}>
+          <input type="text" name='username'  onChange={this.hanldeChange} className="login__email" placeholder="enter your username"/><br/>
+          <input type="password" name='password' onChange={this.hanldeChange} className="login__password" placeholder="enter your password"/><br/>
+          <button className="login__btn">Login</button>
+        </form>
+      </div>
+    </React.Fragment>
   );
-};
+  }
+}
+
 
 export default Login;
