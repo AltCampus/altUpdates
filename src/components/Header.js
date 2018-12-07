d1a298415088824417b871d11ee91f8ed1e424dc
kml1326 <kmlraj131996@gmail.com>
2018-12-08 01:51:30 +0530

    some changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ class Header extends Component {
       const { fullName } = this.props.data;
       profile =
         <Link to='/profile' className="profile-logo">
-          <img className="user-profile" src={require('./../tempImg/praveen.jpg')}  />
+          <img className="user-profile" src={require('./../tempImg/avatar.png')}  />
           <label className="profile-lebel">{fullName}</label>
         </Link>
 
