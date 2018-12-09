81f67dcd22ae526252492f27c2eece5f12637d09
mammothgazillionaire <mammothgazillionaire@gmail.com>
2018-12-09 06:40:33 +0530

    mvc on server side

--- mvc/router/router.js
+++ mvc/router/router.js
@@ -6,12 +6,8 @@ const Updates = require('../controller/Update.controller');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  // User.find({username : 'ertyuio'}, (err, data) => {
-  // 	if(err) return res.sendStatus(404);
-
-  // 	console.log(data)
-  // })
-  res.send("You are connected to Praveen");
+ 
+  res.send("runnin on localhost 8000");
 });
 
 router.post('/signup', User.signup);
