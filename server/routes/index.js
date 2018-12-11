563e6f47e106d601e11f3f28eddfa63457a3068f
prank7 <prashant.abhishek7g@gmail.com>
2018-12-11 14:00:33 +0530

    add API endpoints and bcrypt

---
+++ server/routes/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const user = require('../controllers/user.controller');
+const updates = require('../controllers/update.controller');
+const passport = require('passport');
+// const passport = require('./../modules/passport');
+
+var router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.get('/login', (req, res) => {
+  res.render('index');
+});
+
+module.exports = router;
